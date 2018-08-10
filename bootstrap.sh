6a7a512818d8f246aad8863fcad7ff3f90ad1b75
abe-ma <mas.abe@opt.ne.jp>
2018-08-10 11:18:01 +0900

    standart output core-site.xml change

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,4 +5,8 @@ sed -e "s|AWS_REGION|$AWS_REGION|g" /root/core-site.xml.base > ./conf/core-site.
 sed -i -e "s|S3_ACCESS_KEY|$S3_ACCESS_KEY|g" ./conf/core-site.xml
 sed -i -e "s|S3_SECRET_KEY|$S3_SECRET_KEY|g" ./conf/core-site.xml
 
+# log output
+cat ./conf/core-site.xml
+
+# start apache drill
 ./bin/drill-embedded
